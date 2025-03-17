3c9344813d18395b5c11c268ec39fa61be3d5e0b
Steven Harding <sharding1012@gmail.com>
2025-03-17 11:41:05 -0700

    add auth integration tests

--- src/routes/v1/auth.spec.ts
+++ src/routes/v1/auth.spec.ts
@@ -8,15 +8,46 @@ describe('Auth routes', () => {
     it('should return a 201 status code if given the correct user structure and the user does not exist yet', async () => {
       // Arrange
       const newUser = {
-        email: 'example1234@example.com',
+        email: 'example1@example.com',
         password: 'test-password',
       };
 
       // Act
-      const result = await request(app).post('/api/v1/auth/register').send(newUser);
+      const { status } = await request(app).post('/api/v1/auth/register').send(newUser);
 
       // Assert
-      expect(result.status).toEqual(201);
+      expect(status).toEqual(201);
+    });
+
+    it('should return a 400 status code if request is missing new user information', async () => {
+      // Act
+      const { status } = await request(app).post('/api/v1/auth/register').send({});
+
+      // Assert
+      expect(status).toEqual(400);
+    });
+
+    it('should return a 400 status code if user already exists', async () => {
+      // Arrange
+      const newUser = {
+        email: 'example2@example.com',
+        password: 'test-password',
+      };
+
+      // Act
+
+      // We need to create the user first
+      await request(app).post('/api/v1/auth/register').send(newUser);
+
+      // Try to create the user again
+      const result = await request(app).post('/api/v1/auth/register').send(newUser);
+
+      const { error, code } = result.body;
+
+      // Assert
+      expect(result.status).toEqual(400);
+      expect(error).toEqual('Bad Request');
+      expect(code).toEqual(400);
     });
   });
 });
